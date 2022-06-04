9ca230d27abbcbc184ab1ab3d0fff4f01f508c14
Burak <burak@90pixel.com>
2022-06-04 22:31:14 +0300

    Destructure member types.

--- types.ts
+++ types.ts
@@ -1,23 +1,18 @@
+export interface IUser {
+  id: number;
+  username: string;
+  email: string;
+  color: string;
+  profilePicture: string | null;
+  initials: string;
+  role: number;
+  custom_role: null;
+  last_active: string;
+  date_joined: string;
+  date_invited: string;
+}
+
 export interface IMember {
-  user: {
-    id: number;
-    username: string;
-    email: string;
-    color: string;
-    profilePicture: string | null;
-    initials: string;
-    role: number;
-    custom_role: null;
-    last_active: string;
-    date_joined: string;
-    date_invited: string;
-  };
-  invited_by?: {
-    id: number;
-    username: string;
-    color: string;
-    email: string;
-    initials: string;
-    profilePicture: string;
-  };
+  user: IUser;
+  invited_by?: IUser;
 }
