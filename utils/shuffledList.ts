172a2d01b63aa741ba815f474c40e50e4c000b44
Burak <burak@90pixel.com>
2022-06-04 22:37:31 +0300

    Remove unnecessary comments.

--- utils/shuffledList.ts
+++ utils/shuffledList.ts
@@ -1,25 +1,5 @@
 import { memberList } from './memberData';
 
-// const shuffledMembers = () => {
-//   if (members) {
-//     return [...members].sort(() => 0.5 - Math.random());
-//   }
-// };
-
-// export const list = shuffledMembers();
-
-// export let listText: string;
-
-// list?.forEach((member, index) => {
-//   if (list[index + 1]) {
-//     // console.log(`${member} -> ${list[index + 1]}`);
-//     listText = `${member} -> ${list[index + 1]}`;
-//   } else {
-//     // console.log(`${member} -> ${list[0]}`);
-//     listText = `${member} -> ${list[0]}`;
-//   }
-// });
-
 const shuffledList = (): {
   shuffeledMembers: string[];
   shuffleText: string;
