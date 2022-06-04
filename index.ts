692549e21e620438aae5ce820dc03347af87a7d3
Burak <burak@90pixel.com>
2022-06-04 22:35:10 +0300

    Add stucture to fetch users data after accepting the list.

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
-import { fetcher, shuffeledList, promptLoop } from 'utils';
-import { IMember } from './types';
+import { fetcher, shuffeledList, promptLoop, memberData } from 'utils';
 
 // NOTE: Geçen haftanın listesini çekip; onunla karşılaştırma yaparak
 // listeyi geçen haftadan farklı yapmayı deneyebiliiriz
-
-const { shuffeledMembers, shuffleText } = shuffeledList();
 
 // fetcher({
 //   url: 'https://api.clickup.com/api/v2/team'
@@ -14,6 +11,8 @@ const { shuffeledMembers, shuffleText } = shuffeledList();
 //   );
 // });
 
+const { shuffeledMembers, shuffleText } = shuffeledList();
+
 const conditionText = `Liste bu: ${shuffleText}
 Eğer listeyi beğenmezsen listeyi yenilemek için rs yaz. Eğer listeyi beğendiysen E yaz ve tasklar oluşsun :)`;
 
@@ -21,6 +20,11 @@ async function run() {
   for await (const answer of promptLoop(conditionText)) {
     if (answer === 'E') {
       console.log('proceed with this list', shuffeledMembers);
+
+      console.log('fetching teamMembers started');
+      const teamList = await memberData();
+      console.log(teamList);
+      console.log('fetching teamMembers finished');
 
       break;
     }
