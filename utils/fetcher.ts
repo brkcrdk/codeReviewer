b794f98bf1c66100a85cfe49d3b0e6b83bdc8865
Burak <burak@90pixel.com>
2022-06-06 11:05:41 +0300

    Add headerOptions and requestOptions props to fetcher method.

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,18 +1,22 @@
 import dotenv from 'dotenv';
-import fetch from 'node-fetch';
+import fetch, { HeadersInit, RequestInit } from 'node-fetch';
 
 dotenv.config();
 
 interface IFetcher {
   url: string;
+  headerOptions?: HeadersInit;
+  requestOptions?: RequestInit;
 }
 
-const fetcher = async ({ url }: IFetcher) => {
+const fetcher = async ({ url, headerOptions, requestOptions }: IFetcher) => {
   const request = await fetch(`https://api.clickup.com/api/v2${url}`, {
     headers: {
       Authorization: String(process.env.API_TOKEN),
-      'Content-Type': 'application / json'
-    }
+      'Content-Type': 'application/json',
+      ...headerOptions
+    },
+    ...requestOptions
   });
   const response = await request.json();
   return response;
