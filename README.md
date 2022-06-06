2c4805ae33f6986c2621534532107b9908797cb5
Burak <burak@90pixel.com>
2022-06-06 12:40:23 +0300

    Add list description.

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 * [x] <s>Liste eklenecek klasör idsini env dosyasına ekle.</s>
 * [x] <s>Listeye task eklemeyi test et.</s>
 * [x] <s>Listeye verilecek isimde görüntülenecek tarih aralığı formatını oluştur. Beklenen format şu şekilde: `04.06 - 09.06.2022 Code Review Listesi`</s>
-* [ ] Eklenince, tarih aralığına göre liste adını oluştur.
+* [x] <s>Listenin açıklama kısmını ekle.</s>
+* [x] Eklenince, tarih aralığına göre liste adını oluştur.
 * [ ] O listeye oluşturulan task listesine göre taskları oluştur.
 * [ ] O taska kişileri assign et.
 * [ ] GELİŞTİRME: Geçen haftanın listesini çekip; onunla karşılaştırma yaparak listeyi geçen haftadan farklı yapmayı deneyebiliiriz
--- api/createList.ts
+++ api/createList.ts
@@ -17,13 +17,17 @@ interface CreateListProps {
  */
 
 const createList = async ({ listName }: CreateListProps) => {
+  // liste oluştuğu zaman bu description alanında bu text yer alır.
+  const content =
+    "Tasklar otomatik oluşturulur ve ilişkili kişiler o taska assign edilir. \n\n`Review talebinde bulunan  ➝ Review yapacak olan`\n\nTalepte bulunan kişi bulunduğu taskın altında her bir review için subtask açar. \n\nÖrnek: Pazartesi ➝ https://gitlab.90pixel.net/...\n\nBu subtaska taskı açtığı gün ve açılmış mrın linkini ekler ve review'ı yapacak olan kişiyi o subtaska assign eder. Bu taskın status'u açıldığı zaman todo olmalıdır. Review'ı yapan kişi review tamamlandıktan sonra taskı completed'a çeker ve böylece review işlemi tamamlanmış olur.";
+
   const taskRequest: Promise<IList> = await fetcher({
     url: `/folder/${process.env.FOLDER_ID}/list`,
     requestOptions: {
       method: 'POST',
       body: JSON.stringify({
         name: listName,
-        content: 'New List Content'
+        content
       })
     }
   });
