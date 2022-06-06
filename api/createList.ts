a9a5f1b62e6064140ae404c28130e8d7d82ade95
Burak <burak@90pixel.com>
2022-06-06 12:23:18 +0300

    Update comments

--- api/createList.ts
+++ api/createList.ts
@@ -6,10 +6,14 @@ interface CreateListProps {
 }
 
 /**
- * `listName`'i parametre olarak alıyorum çünkü o alanı her bir task oluşumunda
+ * `listName`'i parametre olarak alıyorum çünkü o alanı her bir liste oluşumunda
  * değiştiriyorum `content` kısmını sabit bir değer olarak bırakıyorum.
  * Burada oluşturulacak listenin description alanına denk geliyor ve ben
  * buraya taskların ne şekilde çalıştığını açıklayıcı bir yazı ekliyorum.
+ *
+ * Ben listeyi oluştururken, task yapılacak haftanın tarih aralığını vermeyi tercih
+ * ediyorum. Bu nedenle oluşturulacak her liste o günün tarihi ve sonraki 4 gün
+ * aralığını içeren isim formatı ile oluşturulmuş olacak.
  */
 
 const createList = async ({ listName }: CreateListProps) => {
