cdc329d180d3d17eff514e309bbfa38807c6f48d
Burak <burak@90pixel.com>
2022-07-02 16:46:35 +0300

    Add lastTaskList api to get latest task list.

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
 export { default as memberData } from './memberData';
 export { default as createList } from './createList';
 export { default as createTask } from './createTask';
+export { default as latestTaskList } from './latestTaskList';
