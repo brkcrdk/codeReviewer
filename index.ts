0a339863dbe39d25bcef74be38c868048513186e
Burak <burak@90pixel.com>
2022-07-03 00:19:07 +0300

    Take back to initial state

--- index.ts
+++ index.ts
@@ -1,69 +1,63 @@
 import { shuffeledList, promptLoop, listNameBuilder } from 'utils';
-import { memberData, createList, createTask, latestTaskList } from 'api';
+import { memberData, createList, createTask } from 'api';
 
-// const { shuffeledMembers, shuffleText } = shuffeledList();
+const { shuffeledMembers, shuffleText } = shuffeledList();
 
-// const conditionText = `Liste bu:
-// ${shuffleText}
-// Eğer listeyi beğenmezsen listeyi yenilemek için rs yaz. Eğer listeyi beğendiysen E yaz ve tasklar oluşsun :)`;
+const conditionText = `Liste bu:
+${shuffleText}
+Eğer listeyi beğenmezsen listeyi yenilemek için rs yaz. Eğer listeyi beğendiysen E yaz ve tasklar oluşsun :)`;
 
-// async function run() {
-//   for await (const answer of promptLoop(conditionText)) {
-//     if (answer === 'E') {
-//       // console.log('Liste oluşturulmaya başlandı');
-//       // const listName = listNameBuilder();
-//       // const { id: listId } = await createList({ listName });
-//       // console.log(`liste ${listName} adıyla oluşturuldu`);
+async function run() {
+  for await (const answer of promptLoop(conditionText)) {
+    if (answer === 'E') {
+      console.log('Liste oluşturulmaya başlandı');
+      const listName = listNameBuilder();
+      const { id: listId } = await createList({ listName });
+      console.log(`liste ${listName} adıyla oluşturuldu`);
 
-//       // console.log('Takım üyelerinin bilgileri toplanıyor.');
-//       const teamList = await memberData();
-//       // console.log('Takım üyelerinin bilgileri toplandı.');
+      console.log('Takım üyelerinin bilgileri toplanıyor.');
+      const teamList = await memberData();
+      console.log('Takım üyelerinin bilgileri toplandı.');
 
-//       // console.log('Tasklar oluşturulmaya başlandı.');
+      console.log('Tasklar oluşturulmaya başlandı.');
 
-//       for (let i = 0; i < shuffeledMembers.length; i++) {
-//         const currentPerson = shuffeledMembers[i];
+      for (let i = 0; i < shuffeledMembers.length; i++) {
+        const currentPerson = shuffeledMembers[i];
 
-//         let nextPerson: string;
-//         if (shuffeledMembers[i + 1]) {
-//           nextPerson = shuffeledMembers[i + 1];
-//         } else {
-//           nextPerson = shuffeledMembers[0];
-//         }
+        let nextPerson: string;
+        if (shuffeledMembers[i + 1]) {
+          nextPerson = shuffeledMembers[i + 1];
+        } else {
+          nextPerson = shuffeledMembers[0];
+        }
 
-//         console.log({ nextPerson, currentPerson });
+        console.log({ nextPerson, currentPerson });
 
-//         // const currentMember = teamList.find(
-//         //   (member) => member.username === currentPerson
-//         // );
-//         // const nextMember = teamList.find(
-//         //   (member) => member.username === nextPerson
-//         // );
+        const currentMember = teamList.find(
+          (member) => member.username === currentPerson
+        );
+        const nextMember = teamList.find(
+          (member) => member.username === nextPerson
+        );
 
-//         // if (currentMember && nextMember) {
-//         //   await createTask({
-//         //     listId,
-//         //     taskName: `${currentPerson} -> ${nextPerson}`,
-//         //     assignees: [currentMember?.id, nextMember?.id]
-//         //   });
-//         //   console.log(
-//         //     `${currentPerson} -> ${nextPerson} ikilisi için task oluşturuldu.`
-//         //   );
-//         // }
-//       }
+        if (currentMember && nextMember) {
+          await createTask({
+            listId,
+            taskName: `${currentPerson} -> ${nextPerson}`,
+            assignees: [currentMember?.id, nextMember?.id]
+          });
+          console.log(
+            `${currentPerson} -> ${nextPerson} ikilisi için task oluşturuldu.`
+          );
+        }
+      }
 
-//       console.log('Tasklar başarıyla oluşturuldu.');
+      console.log('Tasklar başarıyla oluşturuldu.');
 
-//       break;
-//     }
-//     break;
-//   }
-// }
-
-// run();
-
-async function test() {
-  const reqTest = await latestTaskList();
+      break;
+    }
+    break;
+  }
 }
 
-test();
+run();
