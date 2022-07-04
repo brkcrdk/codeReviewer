e6bb23195ba9277d2238a52019d655ae884bd545
Burak <burak@90pixel.com>
2022-07-04 23:05:38 +0300

    Remove unnecessary comments.

--- index.ts
+++ index.ts
@@ -1,80 +1,14 @@
-import { shuffeledList, promptLoop, listNameBuilder } from 'utils';
+import { shuffeledList, listNameBuilder } from 'utils';
 import { memberData, createList, createTask, latestTaskList } from 'api';
 
-// const { shuffeledMembers, shuffleText } = shuffeledList();
-
-// const conditionText = `Liste bu:
-// ${shuffleText}
-// Eğer listeyi beğenmezsen listeyi yenilemek için rs yaz. Eğer listeyi beğendiysen E yaz ve tasklar oluşsun :)`;
-
-// async function run() {
-//   for await (const answer of promptLoop(conditionText)) {
-//     if (answer === 'E') {
-//       // console.log('Liste oluşturulmaya başlandı');
-//       // const listName = listNameBuilder();
-//       // const { id: listId } = await createList({ listName });
-//       // console.log(`liste ${listName} adıyla oluşturuldu`);
-
-//       // console.log('Takım üyelerinin bilgileri toplanıyor.');
-//       const teamList = await memberData();
-//       // console.log('Takım üyelerinin bilgileri toplandı.');
-
-//       // console.log('Tasklar oluşturulmaya başlandı.');
-
-//       for (let i = 0; i < shuffeledMembers.length; i++) {
-//         const currentPerson = shuffeledMembers[i];
-
-//         let nextPerson: string;
-//         if (shuffeledMembers[i + 1]) {
-//           nextPerson = shuffeledMembers[i + 1];
-//         } else {
-//           nextPerson = shuffeledMembers[0];
-//         }
-
-//         // const currentMember = teamList.find(
-//         //   (member) => member.username === currentPerson
-//         // );
-//         // const nextMember = teamList.find(
-//         //   (member) => member.username === nextPerson
-//         // );
-
-//         // if (currentMember && nextMember) {
-//         //   await createTask({
-//         //     listId,
-//         //     taskName: `${currentPerson} -> ${nextPerson}`,
-//         //     assignees: [currentMember?.id, nextMember?.id]
-//         //   });
-//         //   console.log(
-//         //     `${currentPerson} -> ${nextPerson} ikilisi için task oluşturuldu.`
-//         //   );
-//         // }
-//       }
-
-//       console.log('Tasklar başarıyla oluşturuldu.');
-
-//       break;
-//     }
-//     break;
-//   }
-// }
-
-// run();
-
-/**
- * İstediğimiz sonucu verene kadar çalışacak bir recursive function
- * Akış şöyle olmalı:
- * - Bu function içerisinde son listeyi al ve oluşturduğun yeni liste ile karşılaştır
- * - Eğer karşılaştırdığın listede çakışma varsa yeniden liste oluştur ve kendini çağır
- * - Eğer çakışma yoksa, o liste ile taskları oluştur ve functiondan çık.
- */
-const countDown = async (list: string[]) => {
+const createTaskList = async (list: string[]) => {
   const reqTest = await latestTaskList();
 
   const hasConflict = reqTest.some((val) => list.includes(val));
 
   if (hasConflict) {
     const { matchList } = shuffeledList();
-    countDown(matchList);
+    createTaskList(matchList);
     console.log('eşleşme yapılıyor..');
   } else {
     console.log('aranan eşleşme bulundu ve liste oluşturulmaya başladı');
@@ -119,4 +53,4 @@ const countDown = async (list: string[]) => {
 };
 
 const { matchList } = shuffeledList();
-countDown(matchList);
+createTaskList(matchList);
