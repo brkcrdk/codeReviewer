15b97b807a68b8fa8b6baaa33ddb96eb8f9659fa
Burak <burak@90pixel.com>
2022-07-04 23:53:32 +0300

    Update list content.

--- api/createList.ts
+++ api/createList.ts
@@ -19,7 +19,7 @@ interface CreateListProps {
 const createList = async ({ listName }: CreateListProps) => {
   // liste oluştuğu zaman bu description alanında bu text yer alır.
   const content =
-    "Tasklar otomatik oluşturulur ve ilişkili kişiler o taska assign edilir. Taskların oluşturulma mantığı şu şekildedir:\n\n Talepte bulunan kişi bulunduğu taskın altında her bir review için subtask açar.   Örnek:\n\n`Review talebinde bulunan  ➝ Review yapacak olan`\n\nBu subtaska taskı açtığı gün ve açılmış MRın linkini ekler ve review'ı yapacak olan kişiyi o subtaska assign eder.  Örnek:\nPazartesi ➝ https://gitlab.90pixel.net/...\n\nBu taskın status'u açıldığı zaman TODO   olmalıdır. Review'ı yapan kişi review tamamlandıktan sonra taskı COMPLETED 'a çeker ve böylece review işlemi tamamlanmış olur.";
+    "Tasklar otomatik oluşturulur ve ilişkili kişiler o taska assign edilir. Taskların oluşturulma mantığı şu şekildedir:\n\n Talepte bulunan kişi bulunduğu taskın altında her bir review için subtask açar.   Örnek:\n\n`Review talebinde bulunan  ➝ Review yapacak olan`\n\nBu subtaska taskı açtığı gün ve açılmış MRın linkini ekler ve review'ı yapacak olan kişiyi o subtaska assign eder.  Örnek:\n`Pazartesi ➝ https://gitlab.90pixel.net/...`\n\nBu taskın status'u açıldığı zaman `TODO` olmalıdır. Review'ı yapan kişi review tamamlandıktan sonra taskı `COMPLETED`'a çeker ve böylece review işlemi tamamlanmış olur.";
 
   const listRequest: Promise<IList> = await fetcher({
     url: `/folder/${process.env.FOLDER_ID}/list`,
